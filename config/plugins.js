53b14a36f58389dddbd3556482ae89ed3687f242
Mubasil <mubasil.emedstore@gmail.com>
2025-08-03 12:34:09 +0530

    Add Images In Cloud

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,25 @@
-// config/plugins.js
+// // config/plugins.js
+// module.exports = ({ env }) => ({
+//   'users-permissions': {
+//     config: {
+//       jwtSecret: env('JWT_SECRET'),
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
     },
   },
 });
